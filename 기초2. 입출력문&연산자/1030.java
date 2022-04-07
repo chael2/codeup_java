a0f30692da03aa1ef721aed5569e305967760434
Jin <91419759+chael2@users.noreply.github.com>
2022-04-08 02:42:06 +0900

    Create 1030.java

---
+++ 기초2. 입출력문&연산자/1030.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		long data = sc.nextLong();
+		System.out.println(data);
+		
+	}
+
+}
+
+
+//long = 64 bits = -9223372036854775808 ~ 9223372036854775807
+//int = 32 bits = -2147483648 ~ 2147483647
