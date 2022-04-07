ef2b88bdd3565783a2e59e877b0ed45e7dc7e1c2
Jin <91419759+chael2@users.noreply.github.com>
2022-04-08 01:01:16 +0900

    Update 1019.java

--- 기초2. 입출력문&연산자/1019.java
+++ 기초2. 입출력문&연산자/1019.java
@@ -1,9 +1,16 @@
 import java.util.Scanner;
- 
+
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String a = sc.next();
-        
-    }
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String data[] = sc.next().split("\\.");
+		
+		int year = Integer.parseInt(data[0]);
+		int month = Integer.parseInt(data[1]);
+		int day = Integer.parseInt(data[2]);
+		
+		System.out.printf("%04d.%02d.%02d", year, month , day);
+	}
+
 }
