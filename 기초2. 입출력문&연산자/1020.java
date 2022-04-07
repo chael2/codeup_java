972855f647f1d934af315b3d1de14b7208b064b5
Jin <91419759+chael2@users.noreply.github.com>
2022-04-08 01:15:22 +0900

    Update 1020.java

--- 기초2. 입출력문&연산자/1020.java
+++ 기초2. 입출력문&연산자/1020.java
@@ -1,7 +1,16 @@
 import java.util.Scanner;
 
-public class Main{
-    public static void main (String[] args) {
-        Scanner sc = new Scanner(System.in);
-    }
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String a = sc.nextLine();
+		a = a.replace("-", "");
+		System.out.println(a);
+	}
+
 }
+
+
+//split 실패
+//replace로 -를 
